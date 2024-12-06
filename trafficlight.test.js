def3b022f6b9337010f0c4d8c0aa83200852b540
leoni <kaupl@bzz.ch>
2024-12-06 13:14:45 +0100

    first commit

---
+++ trafficlight.test.js
@@ -0,0 +1,25 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+});
+
+describe('test suite traffic light', () => {
+    it(`case green light`, () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+});
+
+describe('test suite traffic light', () => {
+    it(`case orange light`, () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+});
+
+describe('test suite traffic light', () => {
+    it(`case undefined light`, () => {
+        expect(app.trafficLight(undefined)).toBe("OUT OF ORDER");
+    });
+});
